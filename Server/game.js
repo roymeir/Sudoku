f44f86b478bfcb1757615305489cee0d04a3cbb7
roymeir <roy.meir@post.idc.ac.il>
2023-06-05 18:34:53 +0300

    First Commit

---
+++ Server/game.js
@@ -0,0 +1,24 @@
+// 9 by 9 array of zeros
+let board = [
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0]
+];
+
+function solve(grid) {
+    for (let row = 0; row < grid.length; row++) {
+        for (let col = 0; col < grid[0].length; col++) {
+            if (grid[row][col] == 0){
+                for (let guess = 0; guess < grid){
+                    
+                }
+            }
+        }
+    }
+}
